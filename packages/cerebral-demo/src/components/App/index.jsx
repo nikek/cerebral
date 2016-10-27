d1354fc6625030947b964bfa083b5ba0f32205ce
Gaspard Bucher <gaspard@lucidogen.io>
2016-10-27 13:03:57 +0200

    WIP: fix routing and menu selection

---
+++ packages/cerebral-demo/src/components/App/index.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable no-script-url */
+import React from 'react'
+import {connect} from 'cerebral/react'
+import Menu from '../Menu'
+import Timer from '../Timer'
+import Workspace from '../Workspace'
+
+export default connect({
+},
+  function Demo (props) {
+    return (
+      <div className='container'>
+        <section className='hero is-dark'>
+          <div className='hero-body' style={{padding: '40px 20px'}}>
+            <h1 className='title'>Cerebral Demo</h1>
+          </div>
+        </section>
+        <section className='section'>
+          <div className='columns'>
+            <Menu />
+            <div className='column'>
+              <Timer />
+              <Workspace />
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+)
